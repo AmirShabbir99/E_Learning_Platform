38b78112a4ffafd85b9726cdbc72899dfae9b4c2
M.Amir Shabbir <m.amirshabbir99@gmail.com>
2025-07-31 14:53:30 +0500

    instructor update

--- client/src/pages/student/HeroSection.jsx
+++ client/src/pages/student/HeroSection.jsx
@@ -16,6 +16,7 @@ const navigate = useNavigate();
 
   return (
     <div className="relative bg-gradient-to-r from-blue-500 to bg-indigo-600 dark:from-gray-800 dark:to-gray-900 py-24 px-4 text-center">
+  <div className="text-[12px] text-gray-400 ">Instructor credentials: admin@admin - password: 321</div>
       <div className="max-w-3xl mx-auto">
         <h1 className="text-white text-4xl font-bold mb-4">
           Find the Best Courses for You
